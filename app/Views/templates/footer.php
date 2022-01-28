4c270193fea4e9d7601dd58de4e0ec2bb8f2e6a8
Alef Gomes <alefpcj@gmail.com>
2022-01-27 22:22:07 -0300

    resolve header and footer

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -18,11 +18,11 @@
   <!-- REQUIRED SCRIPTS -->
 
   <!-- jQuery -->
-  <script src="../theme/plugins/jquery/jquery.min.js"></script>
+  <script src="<?= base_url('theme/plugins/jquery/jquery.min.js') ?>"></script>
   <!-- Bootstrap 4 -->
-  <script src="../theme/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?= base_url('theme/plugins/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
   <!-- AdminLTE App -->
-  <script src="../theme/dist/js/adminlte.min.js"></script>
+  <script src="<?= base_url('theme/dist/js/adminlte.min.js') ?>"></script>
   <!-- alerts  -->
  <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
   </body>
--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -8,15 +8,15 @@
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome Icons -->
-  <link rel="stylesheet" href="../theme/plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="<?= base_url('theme/plugins/fontawesome-free/css/all.min.css') ?>">
   <!-- DataTables -->
-  <link rel="stylesheet" href="../theme/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
-  <link rel="stylesheet" href="../theme/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
-  <link rel="stylesheet" href="../theme/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+  <link rel="stylesheet" href="<?= base_url('theme/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') ?>">
+  <link rel="stylesheet" href="<?= base_url('theme/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') ?>">
+  <link rel="stylesheet" href="<?= base_url('theme/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') ?>">
   <!-- Theme style -->
-  <link rel="stylesheet" href="../theme/dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="<?= base_url('theme/dist/css/adminlte.min.css') ?>">
     <!-- style  -->
-  <link rel="stylesheet" href="../css/styles.css">
+  <link rel="stylesheet" href="<?= base_url('css/styles.css') ?>">
   <!-- alert  -->
   <link rel="stylesheet" href="sweetalert2.min.css">  
   <link rel="shortcut icon" href="https://img.icons8.com/color/96/000000/godtier.png" type="image/x-icon">
